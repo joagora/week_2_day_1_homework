c33b29c5dad642b4785284ee7b77b13c948d6d26
Joanna Gora <joanna.gora@protonmail.com>
2018-07-03 09:59:06 +0100

    added setup

--- part_b/specs/team_spec.rb
+++ part_b/specs/team_spec.rb
@@ -3,55 +3,48 @@ require_relative('../team.rb')
 
 
 class TestStudent < MiniTest::Test
+  def setup
+    @players = ["Butterfly", "Fly", "Caterpillar"]
+    @team = Team.new("Moth hunters", @players, "Bird")
+  end
+
   def test_name
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal("Moth hunters", team.name)
+    assert_equal("Moth hunters", @team.name)
   end
 
   def test_players
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal(players, team.players)
+    assert_equal(@players, @team.players)
   end
 
   def test_coach
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal("Bird", team.coach)
+    assert_equal("Bird", @team.coach)
   end
 
   def test_set_coach
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal("Bird", team.coach)
+    assert_equal("Bird", @team.coach)
   end
 
   def test_add_player
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    team.add_player("Insect")
-    assert_equal(4, team.players.length)
+    @team.add_player("Insect")
+    assert_equal(4, @team.players.length)
   end
 
   def test_check_player__found
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal(true, team.check_player("Fly"))
+    assert_equal(true, @team.check_player("Fly"))
   end
 
   def test_check_player__not_found
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    assert_equal(false, team.check_player("Joanna"))
+    assert_equal(false, @team.check_player("Joanna"))
   end
 
-  def test_add_points
-    players = ["Butterfly", "Fly", "Caterpillar"]
-    team = Team.new("Moth hunters", players, "Bird")
-    team.add_points("win")
-    assert_equal(1, team.points)
+  def test_add_points_win
+    @team.add_points("win")
+    assert_equal(1, @team.points)
   end
 
+  def test_add_points_lose
+    @team.add_points("lose")
+    assert_equal(0, @team.points)
+  end
 
 end
